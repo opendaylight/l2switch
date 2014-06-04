356a400231ddd44ecb453425a3b91a676ab0a541
Alex Fan <railor33@gmail.com>
2014-06-04 14:03:23 -0700

    Handle Q-in-Q case, not just single 802.1Q header.
    Change-Id: Idc4032b2e11376673bc0ed6e94bfebe1577d9c85
    Signed-off-by: Alex Fan <railor33@gmail.com>

--- packethandler/implementation/src/main/java/org/opendaylight/l2switch/decoder/EthernetDecoder.java
+++ packethandler/implementation/src/main/java/org/opendaylight/l2switch/decoder/EthernetDecoder.java
@@ -7,11 +7,14 @@ import org.opendaylight.controller.sal.utils.NetUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.MacAddress;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.EthernetPacket;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.EthernetPacketBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.Header8021qType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.KnownEtherType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.ethernet.packet.grp.Header8021q;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.ethernet.packet.grp.Header8021qBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -22,20 +25,16 @@ public class EthernetDecoder {
   public static final Integer LENGTH_MAX = 1500;
   public static final Integer ETHERTYPE_MIN = 1536;
   public static final Integer ETHERTYPE_8021Q = 0x8100;
-
-  // payload, 0,payload.length * NetUtils.NumBitsInAByte
+  public static final Integer ETHERTYPE_QINQ = 0x9100;
 
   /**
-   * Decode a packet into an EthernetPacket
+   * Decode a RawPacket into an EthernetPacket
    * @param data - data from wire to deserialize
    * @param bitOffset - bit position where packet header starts in data
    * @return
    */
   public static EthernetPacket decode(byte[] data, int bitOffset) throws BufferException {
     EthernetPacketBuilder builder = new EthernetPacketBuilder();
-
-    //0, 48, 96   = start pos
-    //48, 48, 16  = length in bits
 
     //ToDo:  Possibly log these values
       /*if (_logger.isTraceEnabled()) {
@@ -49,14 +48,17 @@ public class EthernetDecoder {
     builder.setDestination(new MacAddress(HexEncode.bytesToHexStringFormat(BitBufferHelper.getBits(data, 0+bitOffset, 48))));
     builder.setSource(new MacAddress(HexEncode.bytesToHexStringFormat(BitBufferHelper.getBits(data, 48+bitOffset, 48))));
 
-    // Deserialize the optional field 802.1Q header
-    Integer nextField = BitBufferHelper.getInt(BitBufferHelper.getBits(data, 96 + bitOffset, 16));
+    // Deserialize the optional field 802.1Q headers
+    Integer nextField = BitBufferHelper.getInt(BitBufferHelper.getBits(data, 96+bitOffset, 16));
     int extraHeaderBits = 0;
-    if (nextField.equals(ETHERTYPE_8021Q)) {
-      // Read 2 more bytes for priority (3bits), drop eligible (1bit), vlan-id (12bits)
-      byte[] vlanBytes = BitBufferHelper.getBits(data, 112+bitOffset, 16);
-
+    ArrayList<Header8021q> headerList = new ArrayList<Header8021q>();
+    while (nextField.equals(ETHERTYPE_8021Q) || nextField.equals(ETHERTYPE_QINQ)) {
       Header8021qBuilder hBuilder = new Header8021qBuilder();
+      hBuilder.setType(Header8021qType.forValue(nextField));
+
+      // Read 2 more bytes for priority (3bits), drop eligible (1bit), vlan-id (12bits)
+      byte[] vlanBytes = BitBufferHelper.getBits(data, 112+bitOffset+extraHeaderBits, 16);
+
       // Remove the sign & right-shift to get the priority code
       hBuilder.setPriorityCode((short)((vlanBytes[0] & 0xff) >> 5));
 
@@ -67,14 +69,18 @@ public class EthernetDecoder {
       vlanBytes[0] = (byte)(vlanBytes[0] & 0x0F);
       hBuilder.setVlan(BitBufferHelper.getInt(vlanBytes));
 
-      // Set 802.1Q header
-      builder.setHeader8021q(Arrays.asList(hBuilder.build()));
+      // Add 802.1Q header to the growing collection
+      headerList.add(hBuilder.build());
 
-      // Reset value of "nextField" to correspond to following 2 bytes for EtherType/Length
-      nextField = BitBufferHelper.getInt(BitBufferHelper.getBits(data, 128+bitOffset, 16));
+      // Reset value of "nextField" to correspond to following 2 bytes for next 802.1Q header or EtherType/Length
+      nextField = BitBufferHelper.getInt(BitBufferHelper.getBits(data, 128+bitOffset+extraHeaderBits, 16));
 
       // 802.1Q header means payload starts at a later position
-      extraHeaderBits = 32;
+      extraHeaderBits += 32;
+    }
+    // Set 802.1Q headers
+    if (!headerList.isEmpty()) {
+      builder.setHeader8021q(headerList);
     }
 
     // Deserialize the EtherType or Length field
--- packethandler/implementation/src/test/java/org/opendaylight/l2switch/decoder/EthernetDecoderTest.java
+++ packethandler/implementation/src/test/java/org/opendaylight/l2switch/decoder/EthernetDecoderTest.java
@@ -10,13 +10,12 @@ package org.opendaylight.controller.sample.l2switch.md.topology;
 import org.junit.Test;
 import org.opendaylight.l2switch.decoder.EthernetDecoder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.EthernetPacket;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.Header8021qType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.types.rev140528.KnownEtherType;
 
 import java.util.Arrays;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.*;
 
 public class EthernetDecoderTest {
 
@@ -68,11 +67,41 @@ public class EthernetDecoderTest {
     assertEquals(e.getEthertype(), KnownEtherType.Ipv6);
     assertNull(e.getLength());
     assertEquals(e.getHeader8021q().size(), 1);
+    assertEquals(e.getHeader8021q().get(0).getType(), Header8021qType.VlanTagged);
     assertEquals(e.getHeader8021q().get(0).getPriorityCode().intValue(), 7);
     assertTrue(e.getHeader8021q().get(0).isDropEligible());
     assertEquals(e.getHeader8021q().get(0).getVlan().intValue(), 4095);
     assertEquals(e.getDestination().getValue(), "01:23:45:67:89:ab");
     assertEquals(e.getSource().getValue(), "cd:ef:01:23:45:67");
     assertTrue(Arrays.equals(e.getPayload(), Arrays.copyOfRange(packet, 18, packet.length)));
+  }
+
+  @Test
+  public void testDecode_IPv6EtherTypeWithQinQ() throws Exception {
+    byte[] packet = {
+      0x01, 0x23, 0x45, 0x67, (byte)0x89, (byte)0xab,
+      (byte)0xcd, (byte)0xef, 0x01, 0x23, 0x45, 0x67,
+      (byte)0x91, 0x00,
+      (byte)0xff, (byte)0xff,
+      (byte)0x81, 0x00,
+      (byte)0xa0, (byte)0x0a,
+      (byte)0x86, (byte)0xdd,
+      0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11
+    };
+    EthernetPacket e = EthernetDecoder.decode(packet, 0);
+    assertEquals(e.getEthertype(), KnownEtherType.Ipv6);
+    assertNull(e.getLength());
+    assertEquals(e.getHeader8021q().size(), 2);
+    assertEquals(e.getHeader8021q().get(0).getType(), Header8021qType.QInQ);
+    assertEquals(e.getHeader8021q().get(0).getPriorityCode().intValue(), 7);
+    assertTrue(e.getHeader8021q().get(0).isDropEligible());
+    assertEquals(e.getHeader8021q().get(0).getVlan().intValue(), 4095);
+    assertEquals(e.getHeader8021q().get(1).getType(), Header8021qType.VlanTagged);
+    assertEquals(e.getHeader8021q().get(1).getPriorityCode().intValue(), 5);
+    assertFalse(e.getHeader8021q().get(1).isDropEligible());
+    assertEquals(e.getHeader8021q().get(1).getVlan().intValue(), 10);
+    assertEquals(e.getDestination().getValue(), "01:23:45:67:89:ab");
+    assertEquals(e.getSource().getValue(), "cd:ef:01:23:45:67");
+    assertTrue(Arrays.equals(e.getPayload(), Arrays.copyOfRange(packet, 22, packet.length)));
   }
 }
