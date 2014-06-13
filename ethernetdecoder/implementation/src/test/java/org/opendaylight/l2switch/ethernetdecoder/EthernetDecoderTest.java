27cc585d167a7c852937fd11e27715da24f8beb1
Alex Fan <railor33@gmail.com>
2014-06-12 22:45:11 -0700

    Move ethernet functionality to its own module "ethernetdecoder."
    Change-Id: Ic3221db9bb71389b156833c1229df2ee69707edc
    Signed-off-by: Alex Fan <railor33@gmail.com>

--- ethernetdecoder/implementation/src/test/java/org/opendaylight/l2switch/ethernetdecoder/EthernetDecoderTest.java
+++ ethernetdecoder/implementation/src/test/java/org/opendaylight/l2switch/ethernetdecoder/EthernetDecoderTest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.l2switch.packethandler.decoders;
+package org.opendaylight.l2switch.ethernetdecoder;
 
 import org.junit.Test;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.BasePacket;
