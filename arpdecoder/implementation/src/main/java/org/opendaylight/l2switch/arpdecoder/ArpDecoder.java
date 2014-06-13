9f1c0c0da7828cf779b742491b1a2e2455f1e8c9
Alex Fan <railor33@gmail.com>
2014-06-12 18:55:36 -0700

    Renaming modules from *packet to *decoder.
    Change-Id: I0f1c891d257dc6b54169014d608cd5eb299ec5d9
    Signed-off-by: Alex Fan <railor33@gmail.com>

---
+++ arpdecoder/implementation/src/main/java/org/opendaylight/l2switch/arpdecoder/ArpDecoder.java
@@ -0,0 +1,89 @@
+package org.opendaylight.l2switch.arpdecoder;
+
+import org.opendaylight.controller.sal.packet.BitBufferHelper;
+import org.opendaylight.controller.sal.packet.BufferException;
+import org.opendaylight.controller.sal.utils.HexEncode;
+import org.opendaylight.l2switch.packethandler.decoders.PacketDecoder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.ArpPacketBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.ArpPacketGrp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.ArpPacketReceived;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.ArpPacketReceivedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.KnownHardwareType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.KnownOperation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.Packet;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.EthernetPacketGrp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.KnownEtherType;
+import org.opendaylight.yangtools.yang.binding.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * ARP (Address Resolution Protocol) Packet Decoder
+ */
+public class ArpDecoder implements PacketDecoder {
+
+  private static final Logger _logger = LoggerFactory.getLogger(ArpDecoder.class);
+
+  /**
+   * Decode an EthernetPacket into an ArpPacket
+   */
+  @Override
+  public Packet decode(Packet packet) {
+    ArpPacketBuilder builder = new ArpPacketBuilder();
+    if(!(packet instanceof EthernetPacketGrp)) return null;
+
+    EthernetPacketGrp ethernetPacket = (EthernetPacketGrp) packet;
+    builder.fieldsFrom(ethernetPacket);
+    byte[] data = ethernetPacket.getEthernetPayload();
+
+    try {
+      // Decode the hardware-type (HTYPE) and protocol-type (PTYPE) fields
+      builder.setHardwareType(KnownHardwareType.forValue(BitBufferHelper.getInt(BitBufferHelper.getBits(data, 0, 16))));
+      builder.setProtocolType(KnownEtherType.forValue(BitBufferHelper.getInt(BitBufferHelper.getBits(data, 16, 16))));
+
+      // Decode the hardware-length and protocol-length fields
+      builder.setHardwareLength(BitBufferHelper.getShort(BitBufferHelper.getBits(data, 32, 8)));
+      builder.setProtocolLength(BitBufferHelper.getShort(BitBufferHelper.getBits(data, 40, 8)));
+
+      // Decode the operation field
+      builder.setOperation(KnownOperation.forValue(BitBufferHelper.getInt(BitBufferHelper.getBits(data, 48, 16))));
+
+      // Decode the address fields
+      int indexSrcProtAdd = 64 + 8 * builder.getHardwareLength();
+      int indexDstHardAdd = indexSrcProtAdd + 8 * builder.getProtocolLength();
+      int indexDstProtAdd = indexDstHardAdd + 8 * builder.getHardwareLength();
+      if(builder.getHardwareType().equals(KnownHardwareType.Ethernet)) {
+        builder.setSourceHardwareAddress(HexEncode.bytesToHexStringFormat(BitBufferHelper.getBits(data, 64, 8 * builder.getHardwareLength())));
+        builder.setDestinationHardwareAddress(HexEncode.bytesToHexStringFormat(BitBufferHelper.getBits(data, indexDstHardAdd, 8 * builder.getHardwareLength())));
+      } else {
+        _logger.debug("Unknown HardwareType -- sourceHardwareAddress and destinationHardwareAddress are not decoded");
+      }
+
+      if(builder.getProtocolType().equals(KnownEtherType.Ipv4) || builder.getProtocolType().equals(KnownEtherType.Ipv6)) {
+        builder.setSourceProtocolAddress(InetAddress.getByAddress(BitBufferHelper.getBits(data, indexSrcProtAdd, 8 * builder.getProtocolLength())).getHostAddress());
+        builder.setDestinationProtocolAddress(InetAddress.getByAddress(BitBufferHelper.getBits(data, indexDstProtAdd, 8 * builder.getProtocolLength())).getHostAddress());
+      } else {
+        _logger.debug("Unknown ProtocolType -- sourceProtocolAddress and destinationProtocolAddress are not decoded");
+      }
+    } catch(BufferException | UnknownHostException e) {
+      _logger.debug("Exception while decoding APR packet", e.getMessage());
+    }
+
+    return builder.build();
+  }
+
+  @Override
+  public Notification buildPacketNotification(Packet arpPacket) {
+    if(arpPacket == null) return null;
+
+    if(arpPacket instanceof ArpPacketGrp) {
+      return new ArpPacketReceivedBuilder((ArpPacketGrp) arpPacket).build();
+    }
+
+    return null;
+  }
+
+}
---
+++ ipv4decoder/implementation/src/main/java/org/opendaylight/l2switch/ipv4decoder/Ipv4DecoderProvider.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.l2switch.ipv4decoder;
+
+import org.opendaylight.controller.sal.binding.api.AbstractBindingAwareConsumer;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.l2switch.packethandler.decoders.PacketDecoderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.PacketType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.PacketPayloadType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.PacketPayloadTypeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.KnownEtherType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ipv4.rev140528.Ipv4PacketReceived;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Ipv4DecoderProvider serves as the Activator for our Ipv4Decoder OSGI bundle.
+ */
+public class Ipv4DecoderProvider extends AbstractBindingAwareConsumer
+    implements AutoCloseable {
+
+  private final static Logger _logger = LoggerFactory.getLogger(Ipv4DecoderProvider.class);
+
+  /**
+   * Setup the IPv4 packet handler.
+   * @param consumerContext The context of the L2Switch.
+   */
+  @Override
+  public void onSessionInitialized(BindingAwareBroker.ConsumerContext consumerContext) {
+    //ToDo: Replace with config subsystem call
+    PacketDecoderService packetDecoderService = consumerContext.getSALService(PacketDecoderService.class);
+    packetDecoderService.registerDecoder(getEthernetIpv4PacketPayloadType(), new Ipv4Decoder(), Ipv4PacketReceived.class);
+  }
+
+  /**
+   * Cleanup the IPv4 packet handler
+   */
+  @Override
+  public void close() {
+  }
+
+  private PacketPayloadType getEthernetIpv4PacketPayloadType() {
+    return new PacketPayloadTypeBuilder()
+      .setPacketType(PacketType.Ethernet)
+      .setPayloadType(KnownEtherType.Ipv4.getIntValue())
+      .build();
+  }
+}
