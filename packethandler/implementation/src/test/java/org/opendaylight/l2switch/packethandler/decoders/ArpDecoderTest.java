31fee56103c8710854d9bd9739338e3fa1abd912
Alex Fan <railor33@gmail.com>
2014-07-01 16:50:57 -0700

    This commit consists of.. (1) Part of Wojciech's changes to ethernet-packet.yang. (2) EthernetDecoder refactoring. (3) ArpDecoder fix to account for the offset field being in bytes.
    Change-Id: I452665e5593e7a5f380ccdf96154633f6a8eea14
    Signed-off-by: Alex Fan <railor33@gmail.com>

--- packethandler/implementation/src/test/java/org/opendaylight/l2switch/packethandler/decoders/ArpDecoderTest.java
+++ packethandler/implementation/src/test/java/org/opendaylight/l2switch/packethandler/decoders/ArpDecoderTest.java
@@ -1,11 +1,24 @@
 package org.opendaylight.l2switch.packethandler.decoders;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.ArpPacketOverEthernetReceived;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.KnownHardwareType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.KnownOperation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.EthernetPacketOverRawReceivedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.KnownEtherType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.ethernet.packet.over.raw.fields.EthernetPacketBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.ethernet.packet.over.raw.fields.RawPacketBuilder;
+
+import static junit.framework.Assert.*;
+
 public class ArpDecoderTest {
- /* ArpDecoder arpDecoder = new ArpDecoder();
 
   @Test
   public void testDecode_RequestIPv4() throws Exception {
-    byte[] payload = {
+    byte[] packet = {
+      0x00, 0x00, 0x00, 0x00, 0x00, // Offset is 5
       0x00, 0x01, // Hardware Type -- Ethernet
       0x08, 0x00, // Protocol Type -- Ipv4
       0x06, // Hardware Length -- 6
@@ -16,18 +29,24 @@ public class ArpDecoderTest {
       (byte) 0xcd, (byte) 0xef, 0x01, 0x23, 0x45, 0x67, // Dest Hardware Address
       0x01, 0x02, 0x03, 0x04 // Dest Protocol Address
     };
-    ArpPacket arp = (ArpPacket)arpDecoder.decode(new EthernetPacketBuilder().setEthernetPayload(payload).build());
-    assertEquals(KnownHardwareType.Ethernet, arp.getHardwareType());
-    assertEquals(KnownEtherType.Ipv4, arp.getProtocolType());
-    assertEquals(6, arp.getHardwareLength().intValue());
-    assertEquals(4, arp.getProtocolLength().intValue());
-    assertEquals(KnownOperation.Request, arp.getOperation());
-    assertEquals("01:23:45:67:89:ab", arp.getSourceHardwareAddress());
-    assertEquals("192.168.0.1", arp.getSourceProtocolAddress());
-    assertEquals("cd:ef:01:23:45:67", arp.getDestinationHardwareAddress());
-    assertEquals("1.2.3.4", arp.getDestinationProtocolAddress());
+    NotificationProviderService mock = Mockito.mock(NotificationProviderService.class);
+    ArpPacketOverEthernetReceived notification = new ArpDecoder(mock).decode(new EthernetPacketOverRawReceivedBuilder()
+      .setEthernetPacket(new EthernetPacketBuilder().setPayloadOffset(5).setPayloadLength(33).build())
+      .setPayload(packet)
+      .build());
+
+    assertEquals(KnownHardwareType.Ethernet, notification.getArpPacket().getHardwareType());
+    assertEquals(KnownEtherType.Ipv4, notification.getArpPacket().getProtocolType());
+    assertEquals(6, notification.getArpPacket().getHardwareLength().intValue());
+    assertEquals(4, notification.getArpPacket().getProtocolLength().intValue());
+    assertEquals(KnownOperation.Request, notification.getArpPacket().getOperation());
+    assertEquals("01:23:45:67:89:ab", notification.getArpPacket().getSourceHardwareAddress());
+    assertEquals("192.168.0.1", notification.getArpPacket().getSourceProtocolAddress());
+    assertEquals("cd:ef:01:23:45:67", notification.getArpPacket().getDestinationHardwareAddress());
+    assertEquals("1.2.3.4", notification.getArpPacket().getDestinationProtocolAddress());
   }
 
+  /*
   @Test
   public void testDecode_ReplyIPv4() throws Exception {
     byte[] payload = {
