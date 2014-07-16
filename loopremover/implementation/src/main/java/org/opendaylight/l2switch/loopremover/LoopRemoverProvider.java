8d907e963a33ec9d5c910783f8311b265656f1cd
Amit Mandke <ammandke@cisco.com>
2014-07-15 18:48:36 -0700

    Adding loop remover service. 1. Builds MST based on topology. 2. Updates port status based on redundent links.
    Change-Id: Iab6428c29b8e7cd52607b8ceac81259cdb9d8b62
    Signed-off-by: Amit Mandke <ammandke@cisco.com>

---
+++ loopremover/implementation/src/main/java/org/opendaylight/l2switch/loopremover/LoopRemoverProvider.java
@@ -0,0 +1,50 @@
+package org.opendaylight.l2switch.loopremover;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.sal.binding.api.AbstractBindingAwareConsumer;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.l2switch.loopremover.topology.NetworkGraphImpl;
+import org.opendaylight.l2switch.loopremover.topology.NetworkGraphService;
+import org.opendaylight.l2switch.loopremover.topology.TopologyLinkDataChangeHandler;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by amitmandke on 7/9/14.
+ */
+public class LoopRemoverProvider extends AbstractBindingAwareConsumer
+    implements AutoCloseable {
+
+  private final static Logger _logger = LoggerFactory.getLogger(LoopRemoverProvider.class);
+  private Registration<DataChangeListener> listenerRegistration;
+  private TopologyLinkDataChangeHandler topologyLinkDataChangeHandler;
+
+  /**
+   * Setup the L2Switch.
+   *
+   * @param consumerContext The context of the L2Switch.
+   */
+  @Override
+  public void onSessionInitialized(BindingAwareBroker.ConsumerContext consumerContext) {
+    // Setup FlowWriterService
+    DataBroker dataService = consumerContext.<DataBroker>getSALService(DataBroker.class);
+    NetworkGraphService networkGraphService = new NetworkGraphImpl();
+
+    // Register Topology DataChangeListener
+    this.topologyLinkDataChangeHandler = new TopologyLinkDataChangeHandler(dataService, networkGraphService);
+    listenerRegistration = topologyLinkDataChangeHandler.registerAsDataChangeListener();
+
+  }
+
+  /**
+   * Cleanup the L2Switch
+   */
+  @Override
+  public void close() throws Exception {
+    if(listenerRegistration != null) {
+      listenerRegistration.close();
+    }
+  }
+}
---
+++ loopremover/implementation/src/main/java/org/opendaylight/l2switch/loopremover/topology/NetworkGraphImpl.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.l2switch.loopremover.topology;
+
+import com.google.common.base.Preconditions;
+import edu.uci.ics.jung.algorithms.shortestpath.PrimMinimumSpanningTree;
+import edu.uci.ics.jung.graph.DelegateTree;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Implementation of NetworkGraphService{@link org.opendaylight.l2switch.loopremover.topology.NetworkGraphService}.
+ * It uses Jung graph library internally to maintain a graph and optimum way to return shortest path using
+ * Dijkstra algorithm.
+ */
+public class NetworkGraphImpl implements NetworkGraphService {
+
+  private static final Logger _logger = LoggerFactory.getLogger(NetworkGraphImpl.class);
+
+  Graph<NodeId, Link> networkGraph = null;
+
+  //Enable following lines when shortest path functionality is required.
+  //DijkstraShortestPath<NodeId, Link> shortestPath = null;
+
+  /**
+   * Adds links to existing graph or creates new directed graph with given links if graph was not initialized.
+   *
+   * @param links
+   */
+  @Override
+  public synchronized void addLinks(List<Link> links) {
+    if(links == null || links.isEmpty()) {
+      _logger.info("In addLinks: No link added as links is null or empty.");
+      return;
+    }
+
+    if(networkGraph == null) {
+      networkGraph = new UndirectedSparseGraph<>();
+    }
+
+    for(Link link : links) {
+      NodeId sourceNodeId = link.getSource().getSourceNode();
+      NodeId destinationNodeId = link.getDestination().getDestNode();
+      networkGraph.addVertex(sourceNodeId);
+      networkGraph.addVertex(destinationNodeId);
+      networkGraph.addEdge(link, sourceNodeId, destinationNodeId);
+    }
+
+    /*if(shortestPath == null) {
+      shortestPath = new DijkstraShortestPath<>(networkGraph);
+    } else {
+      shortestPath.reset();
+    }*/
+  }
+
+  /**
+   * removes links from existing graph.
+   *
+   * @param links
+   */
+  @Override
+  public synchronized void removeLinks(List<Link> links) {
+    Preconditions.checkNotNull(networkGraph, "Graph is not initialized, add links first.");
+
+    if(links == null || links.isEmpty()) {
+      _logger.info("In removeLinks: No link removed as links is null or empty.");
+      return;
+    }
+
+    for(Link link : links) {
+      networkGraph.removeEdge(link);
+    }
+    /*if(shortestPath == null) {
+      shortestPath = new DijkstraShortestPath<>(networkGraph);
+    } else {
+      shortestPath.reset();
+    }*/
+
+  }
+
+  /**
+   * returns a path between 2 nodes. Uses Dijkstra's algorithm to return shortest path.
+   * @param sourceNodeId
+   * @param destinationNodeId
+   * @return
+   */
+  //@Override
+  /*public synchronized List<Link> getPath(NodeId sourceNodeId, NodeId destinationNodeId) {
+    Preconditions.checkNotNull(shortestPath, "Graph is not initialized, add links first.");
+
+    if(sourceNodeId == null || destinationNodeId == null) {
+      _logger.info("In getPath: returning null, as sourceNodeId or destinationNodeId is null.");
+      return null;
+    }
+
+    return shortestPath.getPath(sourceNodeId, destinationNodeId);
+  }*/
+
+  /**
+   * Clears the prebuilt graph, in case same service instance is required to process a new graph.
+   */
+  @Override
+  public synchronized void clear() {
+    networkGraph = null;
+    //shortestPath = null;
+  }
+
+  /**
+   * Forms MST(minimum spanning tree) from network graph and returns links that are not in MST.
+   *
+   * @return
+   */
+  @Override
+  public synchronized List<Link> getLinksInMst() {
+    List<Link> linksInMst = new ArrayList<>();
+    PrimMinimumSpanningTree<NodeId, Link> networkMst = new PrimMinimumSpanningTree<>(DelegateTree.<NodeId, Link>getFactory());
+    Graph<NodeId, Link> mstGraph = networkMst.transform(networkGraph);
+    Collection<Link> mstLinks = mstGraph.getEdges();
+    linksInMst.addAll(mstLinks);
+    return linksInMst;
+  }
+
+  @Override
+  public List<Link> getAllLinks() {
+    List<Link> allLinks = new ArrayList<>();
+    allLinks.addAll(networkGraph.getEdges());
+    return allLinks;
+  }
+}
