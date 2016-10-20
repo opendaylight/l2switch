39af7bfaf72b7cdf4aa0ca2d007f99ed73727165
Sai MarapaReddy <sai.marapareddy@gmail.com>
2016-10-19 21:50:30 -0700

    BUG 6675 - Adding Loggers for easy debugging branch
    Change-Id: I2e3353c09f613ab8b96be18de23abbb62299cea5
    Signed-off-by: Sai MarapaReddy <sai.marapareddy@gmail.com>

--- addresstracker/implementation/src/main/java/org/opendaylight/l2switch/addresstracker/addressobserver/AddressObservationWriter.java
+++ addresstracker/implementation/src/main/java/org/opendaylight/l2switch/addresstracker/addressobserver/AddressObservationWriter.java
@@ -139,14 +139,21 @@ public class AddressObservationWriter {
                 // Search for this mac-ip pair in the existing address
                 // observations & update last-seen timestamp
                 addresses = acnc.getAddresses();
+                LOG.info("Address observations exist, number of addresses that already exist is {}", addresses.size());
                 for (int i = 0; i < addresses.size(); i++) {
+                    LOG.info("Address observed with ip {} and mac {}",addresses.get(i).getIp(),addresses.get(i).getMac());
                     if (addresses.get(i).getIp().equals(ipAddress) && addresses.get(i).getMac().equals(macAddress)) {
+                        LOG.info("Matching Ip {} & Mac {} observed", ipAddress, macAddress);
+                        LOG.info("Last seen interval in ms {}",now - addresses.get(i).getLastSeen());
                         if ((now - addresses.get(i).getLastSeen()) > timestampUpdateInterval) {
+                            LOG.info("Threshold for timestampUpdateInterval reached ");
                             addressBuilder.setFirstSeen(addresses.get(i).getFirstSeen())
                                     .setKey(addresses.get(i).getKey());
+                            LOG.info("Address {} will be removed",addresses.get(i));
                             addresses.remove(i);
                             break;
                         } else {
+                            LOG.info("Threshold for timestampUpdateInterval NOT reached ");
                             return;
                         }
                     }
