0390059eebf3871fec3b19b28446b9cd6e7d99b5
David Arjona <darjona@yahoo.com>
2024-02-25 14:12:39 -0500

    Fix compatibility methods warnings at hosttracker-impl
    Four warning messages were fixed. 3 at Host.java that required List/Map conversion, and 1 at SimpleAddressObserver.java that required a Uint64 conversion.
    
    Signed-off-by: David Arjona <darjona@yahoo.com>
    Change-Id: I4f6e704be94f744f9d00d476f2628212fb02bf79

--- hosttracker/implementation/src/main/java/org/opendaylight/l2switch/hosttracker/plugin/internal/SimpleAddressObserver.java
+++ hosttracker/implementation/src/main/java/org/opendaylight/l2switch/hosttracker/plugin/internal/SimpleAddressObserver.java
@@ -32,6 +32,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ipv4.rev140528.ipv4.
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ipv6.rev140528.Ipv6PacketListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ipv6.rev140528.Ipv6PacketReceived;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ipv6.rev140528.ipv6.packet.received.packet.chain.packet.Ipv6Packet;
+import org.opendaylight.yangtools.yang.common.Uint64;
 
 /**
  * A Simple Address Observer based on l2switch address observer.
@@ -187,7 +188,9 @@ public class SimpleAddressObserver implements ArpPacketListener, Ipv4PacketListe
          */
         BigInteger id = BigInteger.valueOf(ketype.getIntValue()).abs()
                 .add(BigInteger.valueOf(srcMacAddr.hashCode()).abs().shiftLeft(16));
-        addrs.setId(id);
+        Uint64 newid = Uint64.valueOf(id);
+
+        addrs.setId(newid);
         addrs.withKey(new AddressesKey(addrs.getId()));
         addrs.setVlan(vlanId);
         addrs.setIp(srcIpAddr);
