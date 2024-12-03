485a5712d35b55069f1d9cc89ec75be5ffe62173
David Arjona <darjona@yahoo.com>
2024-12-03 11:47:10 -0500

    Migrate loopremover to binding.api.RpcService
    Use plain AddFlow RPC instead of deprecated SalFlowService at
    l2switch-main and loopremover. RpcConsumerRegistry is used, since
    RpcService has also been deprecated.
    
    Change-Id: Idbf298f73c6e658f6eccc4aa278fbc99f3a1c7a5
    Signed-off-by: David Arjona <darjona@yahoo.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/flow/InitialFlowWriterTest.java
+++ loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/flow/InitialFlowWriterTest.java
@@ -17,14 +17,13 @@ import java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.opendaylight.mdsal.binding.api.DataObjectModification;
 import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
 import org.opendaylight.mdsal.binding.api.DataTreeModification;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
@@ -33,18 +32,20 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.N
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class InitialFlowWriterTest {
-
     @Mock
-    private SalFlowService salFlowService;
+    private AddFlow addFlow;
+    @Mock
+    private DataTreeModification<Node> mockChange;
+    @Mock
+    private DataObjectModification<Node> mockModification;
     private InitialFlowWriter initialFlowWriter;
 
     @Before
     public void initMocks() {
         MockitoAnnotations.initMocks(this);
-        initialFlowWriter = new InitialFlowWriter(salFlowService);
+        initialFlowWriter = new InitialFlowWriter(addFlow);
     }
 
-    @SuppressWarnings("unchecked")
     @Test
     public void onDataChange_Valid() throws Exception {
         InstanceIdentifier<Node> instanceId = InstanceIdentifier.builder(Nodes.class)
@@ -52,8 +53,6 @@ public class InitialFlowWriterTest {
                 .build();
         Node topoNode = new NodeBuilder().setId(new NodeId("openflow:1")).build();
 
-        DataTreeModification<Node> mockChange = Mockito.mock(DataTreeModification.class);
-        DataObjectModification<Node> mockModification = Mockito.mock(DataObjectModification.class);
         when(mockModification.getDataAfter()).thenReturn(topoNode);
         when(mockModification.getModificationType()).thenReturn(DataObjectModification.ModificationType.WRITE);
         when(mockChange.getRootPath()).thenReturn(DataTreeIdentifier.create(LogicalDatastoreType.CONFIGURATION,
@@ -62,6 +61,6 @@ public class InitialFlowWriterTest {
 
         initialFlowWriter.onDataTreeChanged(Collections.singletonList(mockChange));
         Thread.sleep(250);
-        verify(salFlowService, times(1)).addFlow(any(AddFlowInput.class));
+        verify(addFlow, times(1)).invoke(any(AddFlowInput.class));
     }
 }
