0b24f617e699194f5ee3b1a449a91f1d35faf0a7
Robert Varga <robert.varga@pantheon.tech>
2025-02-10 21:13:20 +0100

    Migrate InitialFlowWriterTest to JUnit5
    This is a simple mockito-based test, so:
    - switch to jupiter.api.Test
    - use @ExtendWith(MockitoExtension) to make @Mocks initialized
    - fixup mocking to match expectations
    - use Mockito.timeout() to handle waiting for a single invocation
    
    Also perform some Java 11-based cleanups.
    
    Change-Id: I47b35489eb2c2faabbe476a0813b623c6054ff75
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/flow/InitialFlowWriterTest.java
+++ loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/flow/InitialFlowWriterTest.java
@@ -5,33 +5,32 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.l2switch.loopremover.flow;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
-import org.junit.Before;
-import org.junit.Test;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendaylight.mdsal.binding.api.DataObjectModification;
 import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
 import org.opendaylight.mdsal.binding.api.DataTreeModification;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlow;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
-public class InitialFlowWriterTest {
+@ExtendWith(MockitoExtension.class)
+class InitialFlowWriterTest {
     @Mock
     private AddFlow addFlow;
     @Mock
@@ -40,27 +39,24 @@ public class InitialFlowWriterTest {
     private DataObjectModification<Node> mockModification;
     private InitialFlowWriter initialFlowWriter;
 
-    @Before
-    public void initMocks() {
-        MockitoAnnotations.initMocks(this);
+    @BeforeEach
+    void beforeEach() {
         initialFlowWriter = new InitialFlowWriter(addFlow);
     }
 
     @Test
-    public void onDataChange_Valid() throws Exception {
-        InstanceIdentifier<Node> instanceId = InstanceIdentifier.builder(Nodes.class)
+    void onDataChange_Valid() {
+        var instanceId = InstanceIdentifier.builder(Nodes.class)
                 .child(Node.class, new NodeKey(new NodeId("openflow:1")))
                 .build();
-        Node topoNode = new NodeBuilder().setId(new NodeId("openflow:1")).build();
 
-        when(mockModification.getDataAfter()).thenReturn(topoNode);
+        when(mockModification.getDataBefore()).thenReturn(null);
         when(mockModification.getModificationType()).thenReturn(DataObjectModification.ModificationType.WRITE);
         when(mockChange.getRootPath()).thenReturn(DataTreeIdentifier.create(LogicalDatastoreType.CONFIGURATION,
                 instanceId));
         when(mockChange.getRootNode()).thenReturn(mockModification);
 
-        initialFlowWriter.onDataTreeChanged(Collections.singletonList(mockChange));
-        Thread.sleep(250);
-        verify(addFlow, times(1)).invoke(any(AddFlowInput.class));
+        initialFlowWriter.onDataTreeChanged(List.of(mockChange));
+        verify(addFlow, timeout(250)).invoke(any());
     }
 }
