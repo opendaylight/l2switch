c3fb110b4d6ad2cde316aee957add98aeef70f7a
i-am-arathypm <arathymadhusoodananpn@gmail.com>
2025-07-11 10:10:14 +0530

    l2switch main fixed

--- l2switch-main/src/test/java/org/opendaylight/l2switch/flow/InventoryReaderTest.java
+++ l2switch-main/src/test/java/org/opendaylight/l2switch/flow/InventoryReaderTest.java
@@ -40,9 +40,10 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.N
 import org.opendaylight.yang.gen.v1.urn.opendaylight.l2switch.loopremover.rev140714.StpStatus;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.l2switch.loopremover.rev140714.StpStatusAwareNodeConnector;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.l2switch.loopremover.rev140714.StpStatusAwareNodeConnectorBuilder;
+import org.opendaylight.yangtools.binding.DataObjectIdentifier;
+import org.opendaylight.yangtools.binding.util.BindingMap;
 import org.opendaylight.yangtools.util.concurrent.FluentFutures;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.binding.util.BindingMap;
 import org.opendaylight.yangtools.yang.common.Uint64;
 
 public class InventoryReaderTest {
@@ -56,7 +57,7 @@ public class InventoryReaderTest {
     private Node node;
 
     private InventoryReader inventoryReader;
-    private InstanceIdentifier<Node> nodeInstanceIdentifier;
+    private DataObjectIdentifier<Node> nodeInstanceIdentifier;
 
     @Before
     public void init() {
@@ -67,7 +68,7 @@ public class InventoryReaderTest {
     @Test
     public void getNodeConnectorTest() throws Exception {
 
-        nodeInstanceIdentifier = InstanceIdentifier.builder(Nodes.class)
+        nodeInstanceIdentifier = DataObjectIdentifier.builder(Nodes.class)
                 .child(Node.class, new NodeKey(new NodeId("node-id"))).build();
         when(dataService.newReadOnlyTransaction()).thenReturn(readOnlyTransaction);
         when(readOnlyTransaction.read(any(LogicalDatastoreType.class), any(InstanceIdentifier.class)))
