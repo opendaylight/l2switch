edf2c23ace590eeaa6cfd24b5c30cf93b7c61c9f
Robert Varga <robert.varga@pantheon.tech>
2025-11-28 23:56:37 +0100

    Do not use HexEncode in EthernetDecoder
    IetfYangUtil.macAddressFor() performs the same function, except in a
    much more performant way and guaranteed to use the canonical
    (lower-case) representation.
    
    Change-Id: I3766459fc21b52472300fa9aeabf645c0b3b1d28
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- packethandler/implementation/src/main/java/org/opendaylight/l2switch/packethandler/decoders/EthernetDecoder.java
+++ packethandler/implementation/src/main/java/org/opendaylight/l2switch/packethandler/decoders/EthernetDecoder.java
@@ -10,12 +10,11 @@ package org.opendaylight.l2switch.packethandler.decoders;
 import java.util.ArrayList;
 import org.opendaylight.l2switch.packethandler.decoders.utils.BitBufferHelper;
 import org.opendaylight.l2switch.packethandler.decoders.utils.BufferException;
-import org.opendaylight.l2switch.packethandler.decoders.utils.HexEncode;
 import org.opendaylight.l2switch.packethandler.decoders.utils.NetUtils;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.mdsal.binding.api.NotificationService;
 import org.opendaylight.mdsal.binding.api.NotificationService.Listener;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.IetfYangUtil;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.chain.grp.PacketChain;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.chain.grp.PacketChainBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.chain.grp.packet.chain.packet.RawPacketBuilder;
@@ -87,13 +86,10 @@ public class EthernetDecoder extends AbstractPacketDecoder<PacketReceived, Ether
             .build());
 
         try {
-            EthernetPacketBuilder epBuilder = new EthernetPacketBuilder();
-
-            // Deserialize the destination & source fields
-            epBuilder.setDestinationMac(
-                new MacAddress(HexEncode.bytesToHexStringFormat(BitBufferHelper.getBits(data, 0, 48))));
-            epBuilder.setSourceMac(
-                new MacAddress(HexEncode.bytesToHexStringFormat(BitBufferHelper.getBits(data, 48, 48))));
+            EthernetPacketBuilder epBuilder = new EthernetPacketBuilder()
+                // Deserialize the destination & source fields
+                .setDestinationMac(IetfYangUtil.macAddressFor(BitBufferHelper.getBits(data, 0, 48)))
+                .setSourceMac(IetfYangUtil.macAddressFor(BitBufferHelper.getBits(data, 48, 48)));
 
             // Deserialize the optional field 802.1Q headers
             Integer nextField = BitBufferHelper.getInt(BitBufferHelper.getBits(data, 96, 16));
