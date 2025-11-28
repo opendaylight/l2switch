f5a1afa64c661b079c5350a1f86ded198f481dba
Robert Varga <robert.varga@pantheon.tech>
2025-11-28 19:35:49 +0100

    Migrate ArpPacketHandlerTest
    Use JUnit5 with MockitoExtension. Also inline builders and use List.of()
    instead of an explicit ArrayList.
    
    Change-Id: I8d8bc8c8fdcfecadd9ca7d6c2b0ca28c05cd3654
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- arphandler/src/test/java/org/opendaylight/l2switch/arphandler/core/ArpPacketHandlerTest.java
+++ arphandler/src/test/java/org/opendaylight/l2switch/arphandler/core/ArpPacketHandlerTest.java
@@ -11,99 +11,88 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
-import java.util.ArrayList;
-import org.junit.Before;
-import org.junit.Test;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.ArpPacketReceived;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.ArpPacketReceivedBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.arp.packet.received.packet.chain.packet.ArpPacketBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.chain.grp.PacketChain;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.chain.grp.PacketChainBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.chain.grp.packet.chain.packet.RawPacketBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.basepacket.rev140528.packet.chain.grp.packet.chain.packet.raw.packet.RawPacketFieldsBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.ethernet.packet.received.packet.chain.packet.EthernetPacketBuilder;
 
-public class ArpPacketHandlerTest {
-
+@ExtendWith(MockitoExtension.class)
+class ArpPacketHandlerTest {
     @Mock
     private PacketDispatcher packetDispatcher;
     private ArpPacketHandler arpPacketHandler;
 
-    @Before
-    public void initMocks() {
-        MockitoAnnotations.initMocks(this);
+    @BeforeEach
+    void beforeEach() {
         arpPacketHandler = new ArpPacketHandler(packetDispatcher);
     }
 
     @Test
-    public void onArpPacketReceivedTest() throws Exception {
-        ArrayList<PacketChain> packetChainList = new ArrayList<>();
-        packetChainList.add(new PacketChainBuilder()
-            .setPacket(new RawPacketBuilder().setRawPacketFields(new RawPacketFieldsBuilder().build()).build())
+    void onArpPacketReceivedTest() throws Exception {
+        arpPacketHandler.onNotification(new ArpPacketReceivedBuilder()
+            .setPacketChain(List.of(
+                new PacketChainBuilder()
+                    .setPacket(new RawPacketBuilder().setRawPacketFields(new RawPacketFieldsBuilder().build()).build())
+                .build(),
+                new PacketChainBuilder().setPacket(new EthernetPacketBuilder().build()).build(),
+                new PacketChainBuilder().setPacket(new ArpPacketBuilder().build()).build()))
             .build());
-        packetChainList.add(new PacketChainBuilder().setPacket(new EthernetPacketBuilder().build()).build());
-        packetChainList.add(new PacketChainBuilder().setPacket(new ArpPacketBuilder().build()).build());
-        ArpPacketReceived arpReceived = new ArpPacketReceivedBuilder().setPacketChain(packetChainList).build();
-        arpPacketHandler.onNotification(arpReceived);
-
         verify(packetDispatcher, times(1)).dispatchPacket(null, null, null, null);
     }
 
     @Test
-    public void onArpPacketReceivedTest_NullInput() throws Exception {
+    void onArpPacketReceivedTest_NullInput() throws Exception {
         arpPacketHandler.onNotification(null);
         verifyNoInteractions(packetDispatcher);
     }
 
     @Test
-    public void onArpPacketReceivedTest_NullPacketChain() throws Exception {
-        ArpPacketReceived arpReceived = new ArpPacketReceivedBuilder().build();
-        arpPacketHandler.onNotification(arpReceived);
-
+    void onArpPacketReceivedTest_NullPacketChain() throws Exception {
+        arpPacketHandler.onNotification(new ArpPacketReceivedBuilder().build());
         verifyNoInteractions(packetDispatcher);
     }
 
     @Test
-    public void onArpPacketReceivedTest_EmptyPacketChain() throws Exception {
-        ArrayList<PacketChain> packetChainList = new ArrayList<>();
-        ArpPacketReceived arpReceived = new ArpPacketReceivedBuilder().setPacketChain(packetChainList).build();
-        arpPacketHandler.onNotification(arpReceived);
-
+    void onArpPacketReceivedTest_EmptyPacketChain() throws Exception {
+        arpPacketHandler.onNotification(new ArpPacketReceivedBuilder().setPacketChain(List.of()).build());
         verifyNoInteractions(packetDispatcher);
     }
 
     @Test
-    public void onArpPacketReceivedTest_NoRawPacket() throws Exception {
-        ArrayList<PacketChain> packetChainList = new ArrayList<>();
-        packetChainList.add(new PacketChainBuilder().setPacket(new EthernetPacketBuilder().build()).build());
-        packetChainList.add(new PacketChainBuilder().setPacket(new ArpPacketBuilder().build()).build());
-        ArpPacketReceived arpReceived = new ArpPacketReceivedBuilder().setPacketChain(packetChainList).build();
-        arpPacketHandler.onNotification(arpReceived);
-
+    void onArpPacketReceivedTest_NoRawPacket() throws Exception {
+        arpPacketHandler.onNotification(new ArpPacketReceivedBuilder()
+            .setPacketChain(List.of(
+                new PacketChainBuilder().setPacket(new EthernetPacketBuilder().build()).build(),
+                new PacketChainBuilder().setPacket(new ArpPacketBuilder().build()).build()))
+            .build());
         verifyNoInteractions(packetDispatcher);
     }
 
     @Test
-    public void onArpPacketReceivedTest_NoEthernetPacket() throws Exception {
-        ArrayList<PacketChain> packetChainList = new ArrayList<>();
-        packetChainList.add(new PacketChainBuilder().setPacket(new RawPacketBuilder().build()).build());
-        packetChainList.add(new PacketChainBuilder().setPacket(new ArpPacketBuilder().build()).build());
-        ArpPacketReceived arpReceived = new ArpPacketReceivedBuilder().setPacketChain(packetChainList).build();
-        arpPacketHandler.onNotification(arpReceived);
-
+    void onArpPacketReceivedTest_NoEthernetPacket() throws Exception {
+        arpPacketHandler.onNotification(new ArpPacketReceivedBuilder()
+            .setPacketChain(List.of(
+                new PacketChainBuilder().setPacket(new RawPacketBuilder().build()).build(),
+                new PacketChainBuilder().setPacket(new ArpPacketBuilder().build()).build()))
+            .build());
         verifyNoInteractions(packetDispatcher);
     }
 
     @Test
-    public void onArpPacketReceivedTest_NoArpPacket() throws Exception {
-        ArrayList<PacketChain> packetChainList = new ArrayList<>();
-        packetChainList.add(new PacketChainBuilder().setPacket(new RawPacketBuilder().build()).build());
-        packetChainList.add(new PacketChainBuilder().setPacket(new EthernetPacketBuilder().build()).build());
-        ArpPacketReceived arpReceived = new ArpPacketReceivedBuilder().setPacketChain(packetChainList).build();
-        arpPacketHandler.onNotification(arpReceived);
-
+    void onArpPacketReceivedTest_NoArpPacket() throws Exception {
+        arpPacketHandler.onNotification(new ArpPacketReceivedBuilder()
+            .setPacketChain(List.of(
+                new PacketChainBuilder().setPacket(new RawPacketBuilder().build()).build(),
+                new PacketChainBuilder().setPacket(new EthernetPacketBuilder().build()).build()))
+            .build());
         verifyNoInteractions(packetDispatcher);
     }
 }
