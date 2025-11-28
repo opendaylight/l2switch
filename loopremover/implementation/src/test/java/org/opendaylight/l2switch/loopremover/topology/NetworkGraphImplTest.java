9a88a6a8ac0535cd5f240b120c4a7bf94d3dab17
Robert Varga <robert.varga@pantheon.tech>
2025-11-28 23:20:36 +0100

    Modernize NetworkGraphImplTest
    Use JUnit5 and introduce utility methods for building links, which
    reduce verbosity and improve clarity.
    
    Change-Id: I06305cfc7391172cb288f061c1d299a16b7834b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/topology/NetworkGraphImplTest.java
+++ loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/topology/NetworkGraphImplTest.java
@@ -8,11 +8,11 @@
 package org.opendaylight.l2switch.loopremover.topology;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.ArrayList;
 import java.util.List;
-import org.junit.Before;
-import org.junit.Test;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.LinkId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
@@ -21,178 +21,103 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.LinkBuilder;
 
-public class NetworkGraphImplTest {
-
-    private NetworkGraphImpl networkGraphImpl;
-
-    @Before
-    public void initMocks() {
-        networkGraphImpl = new NetworkGraphImpl();
-    }
+class NetworkGraphImplTest {
+    private final NetworkGraphService networkGraphImpl = new NetworkGraphImpl();
 
     @Test
-    public void testClear() throws Exception {
+    void testClear() {
+        // FIXME: test effects
         networkGraphImpl.clear();
     }
 
     @Test
-    public void testGetLinksInMst_NullGraph() throws Exception {
-        networkGraphImpl.clear();
-        List<Link> mstLinks = networkGraphImpl.getLinksInMst();
+    void testGetLinksInMst_NullGraph() {
+        var mstLinks = networkGraphImpl.getLinksInMst();
         assertEquals(0, mstLinks.size());
     }
 
     @Test
-    public void testAllLinks_NullGraph() throws Exception {
-        networkGraphImpl.clear();
-        List<Link> allLinks = networkGraphImpl.getAllLinks();
+    void testAllLinks_NullGraph() {
+        var allLinks = networkGraphImpl.getAllLinks();
         assertEquals(0, allLinks.size());
     }
 
     @Test
-    public void testAddLinks_NullInput() throws Exception {
+    void testAddLinks_NullInput() {
         networkGraphImpl.clear();
         networkGraphImpl.addLinks(null);
-        List<Link> mstLinks = networkGraphImpl.getLinksInMst();
+        var mstLinks = networkGraphImpl.getLinksInMst();
         assertEquals(0, mstLinks.size());
-        List<Link> allLinks = networkGraphImpl.getAllLinks();
+        var allLinks = networkGraphImpl.getAllLinks();
         assertEquals(0, allLinks.size());
     }
 
     @Test
-    public void testAddLinks_ValidInput() throws Exception {
-        Link link1 = new LinkBuilder().setLinkId(new LinkId("openflow:1"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:1"))
-                        .setSourceTp(new TpId("openflow:1")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:2"))
-                        .setDestTp(new TpId("openflow:2")).build())
-                .build();
-        Link link2 = new LinkBuilder().setLinkId(new LinkId("openflow:2"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:2"))
-                        .setSourceTp(new TpId("openflow:2")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:3"))
-                        .setDestTp(new TpId("openflow:3")).build())
-                .build();
-        List<Link> links = new ArrayList<>();
-        links.add(link1);
-        links.add(link2);
+    void testAddLinks_ValidInput() {
+        final var link1 = newLink("openflow:1", "openflow:1", "openflow:1", "openflow:2", "openflow:2");
+        final var link2 = newLink("openflow:2", "openflow:2", "openflow:2", "openflow:3", "openflow:3");
 
-        networkGraphImpl.addLinks(links);
-        List<Link> mstLinks = networkGraphImpl.getLinksInMst();
+        networkGraphImpl.addLinks(List.of(link1, link2));
+        var mstLinks = networkGraphImpl.getLinksInMst();
         assertEquals(2, mstLinks.size());
-        List<Link> allLinks = networkGraphImpl.getAllLinks();
+        var allLinks = networkGraphImpl.getAllLinks();
         assertEquals(2, allLinks.size());
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testRemoveLinks_NullInput() throws Exception {
-        networkGraphImpl.clear();
-        networkGraphImpl.removeLinks(null);
+    @Test
+    void testRemoveLinks_NullInput() {
+        assertThrows(NullPointerException.class, () -> networkGraphImpl.removeLinks(null));
     }
 
     @Test
-    public void testRemoveLinks_ValidInput() throws Exception {
-        Link link1 = new LinkBuilder().setLinkId(new LinkId("openflow:1"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:1"))
-                        .setSourceTp(new TpId("openflow:1")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:2"))
-                        .setDestTp(new TpId("openflow:2")).build())
-                .build();
-        Link link2 = new LinkBuilder().setLinkId(new LinkId("openflow:2"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:2"))
-                        .setSourceTp(new TpId("openflow:2")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:3"))
-                        .setDestTp(new TpId("openflow:3")).build())
-                .build();
-        Link link3 = new LinkBuilder().setLinkId(new LinkId("openflow:3"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:3"))
-                        .setSourceTp(new TpId("openflow:3")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:4"))
-                        .setDestTp(new TpId("openflow:4")).build())
-                .build();
-        List<Link> links = new ArrayList<>();
-        links.add(link1);
-        links.add(link2);
-        links.add(link3);
-        List<Link> removeLinks = new ArrayList<>();
-        removeLinks.add(link1);
+    void testRemoveLinks_ValidInput() {
+        final var link1 = newLink("openflow:1", "openflow:1", "openflow:1", "openflow:2", "openflow:2");
+        final var link2 = newLink("openflow:2", "openflow:2", "openflow:2", "openflow:3", "openflow:3");
+        final var link3 = newLink("openflow:3", "openflow:3", "openflow:3", "openflow:4", "openflow:4");
 
-        networkGraphImpl.addLinks(links);
-        List<Link> mstLinks = networkGraphImpl.getLinksInMst();
+        networkGraphImpl.addLinks(List.of(link1, link2, link3));
+        var mstLinks = networkGraphImpl.getLinksInMst();
         assertEquals(3, mstLinks.size());
-        List<Link> allLinks = networkGraphImpl.getAllLinks();
+        var allLinks = networkGraphImpl.getAllLinks();
         assertEquals(3, allLinks.size());
 
-        networkGraphImpl.removeLinks(removeLinks);
+        networkGraphImpl.removeLinks(List.of(link1));
         allLinks = networkGraphImpl.getAllLinks();
         assertEquals(2, allLinks.size());
     }
 
     @Test
-    public void testMstRemovesLoops() throws Exception {
-        Link link1 = new LinkBuilder().setLinkId(new LinkId("openflow:1:1"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:1"))
-                        .setSourceTp(new TpId("openflow:1:1")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:2"))
-                        .setDestTp(new TpId("openflow:2:1")).build())
-                .build();
-        Link link2 = new LinkBuilder().setLinkId(new LinkId("openflow:2:1"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:2"))
-                        .setSourceTp(new TpId("openflow:2:1")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:1"))
-                        .setDestTp(new TpId("openflow:1:1")).build())
-                .build();
-        Link link3 = new LinkBuilder().setLinkId(new LinkId("openflow:2:2"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:2"))
-                        .setSourceTp(new TpId("openflow:2:2")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:3"))
-                        .setDestTp(new TpId("openflow:3:1")).build())
-                .build();
-        Link link4 = new LinkBuilder().setLinkId(new LinkId("openflow:3:1"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:3"))
-                        .setSourceTp(new TpId("openflow:3:1")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:2"))
-                        .setDestTp(new TpId("openflow:2:2")).build())
-                .build();
-        Link link5 = new LinkBuilder().setLinkId(new LinkId("openflow:3:2"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:3"))
-                        .setSourceTp(new TpId("openflow:3:2")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:1"))
-                        .setDestTp(new TpId("openflow:1:2")).build())
-                .build();
-        Link link6 = new LinkBuilder().setLinkId(new LinkId("openflow:1:2"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:1"))
-                        .setSourceTp(new TpId("openflow:1:2")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:3"))
-                        .setDestTp(new TpId("openflow:3:2")).build())
-                .build();
+    void testMstRemovesLoops() {
+        final var link1 = newLink("openflow:1:1", "openflow:1", "openflow:1:1", "openflow:2", "openflow:2:1");
+        final var link2 = newLink("openflow:2:1", "openflow:2", "openflow:2:1", "openflow:1", "openflow:1:1");
+        final var link3 = newLink("openflow:2:2", "openflow:2", "openflow:2:2", "openflow:3", "openflow:3:1");
+        final var link4 = newLink("openflow:3:1", "openflow:3", "openflow:3:1", "openflow:2", "openflow:2:2");
+        final var link5 = newLink("openflow:3:2", "openflow:3", "openflow:3:2", "openflow:1", "openflow:1:2");
+        final var link6 = newLink("openflow:1:2", "openflow:1", "openflow:1:2", "openflow:3", "openflow:3:2");
         // parallel link betn nodes 1 & 2
-        Link link7 = new LinkBuilder().setLinkId(new LinkId("openflow:1:3"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:1"))
-                        .setSourceTp(new TpId("openflow:1:3")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:2"))
-                        .setDestTp(new TpId("openflow:2:3")).build())
-                .build();
-        Link link8 = new LinkBuilder().setLinkId(new LinkId("openflow:2:3"))
-                .setSource(new SourceBuilder().setSourceNode(new NodeId("openflow:2"))
-                        .setSourceTp(new TpId("openflow:2:3")).build())
-                .setDestination(new DestinationBuilder().setDestNode(new NodeId("openflow:1"))
-                        .setDestTp(new TpId("openflow:1:3")).build())
-                .build();
-        List<Link> links = new ArrayList<>();
-        links.add(link1);
-        links.add(link2);
-        links.add(link3);
-        links.add(link4);
-        links.add(link5);
-        links.add(link6);
-        links.add(link7);
-        links.add(link8);
+        final var link7 = newLink("openflow:1:3", "openflow:1", "openflow:1:3", "openflow:2", "openflow:2:3");
+        final var link8 = newLink("openflow:2:3", "openflow:2", "openflow:2:3", "openflow:1", "openflow:1:3");
 
-        networkGraphImpl.addLinks(links);
-        List<Link> mstLinks = networkGraphImpl.getLinksInMst();
+        networkGraphImpl.addLinks(List.of(link1, link2, link3, link4, link5, link6, link7, link8));
+        var mstLinks = networkGraphImpl.getLinksInMst();
         assertEquals(2, mstLinks.size());
-        List<Link> allLinks = networkGraphImpl.getAllLinks();
+        var allLinks = networkGraphImpl.getAllLinks();
         assertEquals(4, allLinks.size());
+    }
+
+    @NonNullByDefault
+    private static Link newLink(final String linkId, final String srcNode, final String srcTp, final String dstNode,
+            final String dstTp) {
+        return new LinkBuilder()
+            .setLinkId(new LinkId(linkId))
+            .setSource(new SourceBuilder()
+                .setSourceNode(new NodeId(srcNode))
+                .setSourceTp(new TpId(srcTp))
+                .build())
+            .setDestination(new DestinationBuilder()
+                .setDestNode(new NodeId(dstNode))
+                .setDestTp(new TpId(dstTp))
+                .build())
+            .build();
     }
 }
