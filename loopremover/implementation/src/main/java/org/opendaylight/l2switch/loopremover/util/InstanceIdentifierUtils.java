31d3fb43b429adb6799a32ce3f73616a4f7009f6
Robert Varga <robert.varga@pantheon.tech>
2025-11-28 22:03:12 +0100

    Remove unused InstanceIdentifierUtils methods
    We have quite a few methods used only from tests: remove them.
    
    This leaves only two methods, which have their returns sharpened to
    KeyedInstanceIdentifier. The tests are updated to assert equality, so we
    do not end up asserting common InstanceIdentifier methods.
    
    Change-Id: Ia98562eb5fe8f07095cbbd77e521c30578f995fa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- loopremover/implementation/src/main/java/org/opendaylight/l2switch/loopremover/util/InstanceIdentifierUtils.java
+++ loopremover/implementation/src/main/java/org/opendaylight/l2switch/loopremover/util/InstanceIdentifierUtils.java
@@ -7,13 +7,7 @@
  */
 package org.opendaylight.l2switch.loopremover.util;
 
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
@@ -24,9 +18,8 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
-import org.opendaylight.yangtools.binding.DataObjectIdentifier;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.Uint8;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
 
 /**
  * InstanceIdentifierUtils provides utility functions related to InstanceIdentifiers.
@@ -36,111 +29,20 @@ public final class InstanceIdentifierUtils {
         // Hidden on purpose
     }
 
-    /**
-     * Creates an Instance Identifier (path) for node with specified id.
-     *
-     * @param nodeId the node id
-     * @return the node InstanceIdentifier
-     */
-    // FIXME: inline into sole caller
-    public static InstanceIdentifier<Node> createNodePath(final NodeId nodeId) {
-        return InstanceIdentifier.builder(Nodes.class).child(Node.class, new NodeKey(nodeId)).build();
-    }
-
-    /**
-     * Shorten's node child path to node path.
-     *
-     * @param nodeChild
-     *            child of node, from which we want node path.
-     * @return the Node InstanceIdentifier
-     */
-    // FIXME: remove this method
-    public static InstanceIdentifier<Node> getNodePath(final InstanceIdentifier<?> nodeChild) {
-        return nodeChild.firstIdentifierOf(Node.class);
-    }
-
-    /**
-     * Creates a table path by appending table specific location to node path.
-     *
-     * @param nodePath the node path
-     * @param tableKey the table key
-     * @return the table InstanceIdentifier
-     */
-    // FIXME: remove this method
-    public static InstanceIdentifier<Table> createTablePath(final InstanceIdentifier<Node> nodePath,
-            final TableKey tableKey) {
-        return nodePath.builder().augmentation(FlowCapableNode.class).child(Table.class, tableKey).build();
-    }
-
-    /**
-     * Creates a path for particular flow, by appending flow-specific
-     * information to table path.
-     *
-     * @param table the table path
-     * @param flowKey the floe key
-     * @return the flow InstanceIdentifier
-     */
-    // FIXME: remove this method
-    public static InstanceIdentifier<Flow> createFlowPath(final InstanceIdentifier<Table> table,
-            final FlowKey flowKey) {
-        return table.child(Flow.class, flowKey);
-    }
-
-    /**
-     * Extract table id from table path.
-     *
-     * @param tablePath the table path
-     * @return the table id
-     */
-    // FIXME: remove this method
-    public static Uint8 getTableId(final InstanceIdentifier<Table> tablePath) {
-        return tablePath.firstKeyOf(Table.class).getId();
-    }
-
-    /**
-     * Extracts NodeConnectorKey from node connector path.
-     */
-    // FIXME: remove this method
-    public static NodeConnectorKey getNodeConnectorKey(final InstanceIdentifier<?> nodeConnectorPath) {
-        return nodeConnectorPath.firstKeyOf(NodeConnector.class);
-    }
-
-    /**
-     * Extracts NodeKey from node path.
-     */
-    // FIXME: remove this method
-    public static NodeKey getNodeKey(final InstanceIdentifier<?> nodePath) {
-        return nodePath.firstKeyOf(Node.class);
+    // FIXME: move to TopologyDataChangeEventProcessor
+    public static KeyedInstanceIdentifier<NodeConnector, NodeConnectorKey> createNodeConnectorIdentifier(
+            final String nodeIdValue, final String nodeConnectorIdValue) {
+        return InstanceIdentifier.builder(Nodes.class)
+            .child(Node.class, new NodeKey(new NodeId(nodeIdValue)))
+            .child(NodeConnector.class, new NodeConnectorKey(new NodeConnectorId(nodeConnectorIdValue)))
+            .build();
     }
 
     // FIXME: move to TopologyDataChangeEventProcessor
-    public static InstanceIdentifier<NodeConnector> createNodeConnectorIdentifier(final String nodeIdValue,
-            final String nodeConnectorIdValue) {
-        return createNodePath(new NodeId(nodeIdValue)).child(NodeConnector.class,
-                new NodeConnectorKey(new NodeConnectorId(nodeConnectorIdValue)));
-    }
-
-    // FIXME: inline into sole caller
-    public static InstanceIdentifier<Node> generateNodeInstanceIdentifier(final NodeConnectorRef nodeConnectorRef) {
-        return ((DataObjectIdentifier<?>)nodeConnectorRef.getValue()).toLegacy().firstIdentifierOf(Node.class);
-    }
-
-    // FIXME: inline into sole caller
-    public static InstanceIdentifier<Table> generateFlowTableInstanceIdentifier(final NodeConnectorRef nodeConnectorRef,
-            final TableKey flowTableKey) {
-        return generateNodeInstanceIdentifier(nodeConnectorRef).builder().augmentation(FlowCapableNode.class)
-                .child(Table.class, flowTableKey).build();
-    }
-
-    // FIXME: remove this method
-    public static InstanceIdentifier<Flow> generateFlowInstanceIdentifier(final NodeConnectorRef nodeConnectorRef,
-            final TableKey flowTableKey, final FlowKey flowKey) {
-        return generateFlowTableInstanceIdentifier(nodeConnectorRef, flowTableKey).child(Flow.class, flowKey);
-    }
-
-    // FIXME: move to TopologyDataChangeEventProcessor
-    public static InstanceIdentifier<Topology> generateTopologyInstanceIdentifier(final String topologyId) {
+    public static KeyedInstanceIdentifier<Topology, TopologyKey> generateTopologyInstanceIdentifier(
+            final String topologyId) {
         return InstanceIdentifier.builder(NetworkTopology.class)
-                .child(Topology.class, new TopologyKey(new TopologyId(topologyId))).build();
+            .child(Topology.class, new TopologyKey(new TopologyId(topologyId)))
+            .build();
     }
 }
--- loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/util/InstanceIdentifierUtilsTest.java
+++ loopremover/implementation/src/test/java/org/opendaylight/l2switch/loopremover/util/InstanceIdentifierUtilsTest.java
@@ -8,85 +8,34 @@
 package org.opendaylight.l2switch.loopremover.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
-import org.opendaylight.yangtools.binding.DataObjectIdentifier;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.Uint8;
 
 class InstanceIdentifierUtilsTest {
-    private static final Uint8 NUM_ID_1 = Uint8.ONE;
-    private static final String STR_ID_1 = "id1";
-    private static final String STR_ID_2 = "id2";
-
-    @Test
-    void testCreateNodePath() {
-        assertEquals(InstanceIdentifier.builder(Nodes.class)
-            .child(Node.class, new NodeKey(new NodeId(STR_ID_1)))
-            .build(), InstanceIdentifierUtils.createNodePath(new NodeId(STR_ID_1)));
-    }
-
-    @Test
-    void testGetNodePath() {
-        var ncInsId = InstanceIdentifier.builder(Nodes.class).child(Node.class)
-                .child(NodeConnector.class).build();
-        assertNotNull(InstanceIdentifierUtils.getNodePath(ncInsId));
-    }
-
-    @Test
-    void testCreateTablePath() {
-        var tableInsId = InstanceIdentifierUtils.createTablePath(
-                InstanceIdentifier.builder(Nodes.class).child(Node.class).build(), new TableKey(NUM_ID_1));
-        assertNotNull(tableInsId);
-        assertEquals(NUM_ID_1.shortValue(), tableInsId.getFirstKeyOf(Table.class).getId().shortValue());
-        assertNotNull(tableInsId.firstIdentifierOf(FlowCapableNode.class));
-    }
-
     @Test
     void testCreateNodeConnectorIdentifier() {
-        var ncInsId = InstanceIdentifierUtils.createNodeConnectorIdentifier(STR_ID_1, STR_ID_2);
-        assertNotNull(ncInsId);
-        assertEquals(STR_ID_1, ncInsId.getFirstKeyOf(Node.class).getId().getValue());
-        assertEquals(STR_ID_2, ncInsId.getFirstKeyOf(NodeConnector.class).getId().getValue());
-    }
-
-    @Test
-    void testGenerateNodeInstanceIdentifier() {
-        var ncRef = new NodeConnectorRef(DataObjectIdentifier.builder(Nodes.class)
-            .child(Node.class, new NodeKey(new NodeId("abc")))
-            .child(NodeConnector.class, new NodeConnectorKey(new NodeConnectorId("def")))
-            .build());
-        assertNotNull(InstanceIdentifierUtils.generateNodeInstanceIdentifier(ncRef));
-    }
-
-    @Test
-    void testGenerateFlowTableInstanceIdentifier() {
-        var ncRef = new NodeConnectorRef(DataObjectIdentifier.builder(Nodes.class)
-            .child(Node.class, new NodeKey(new NodeId("abc")))
-            .child(NodeConnector.class, new NodeConnectorKey(new NodeConnectorId("def")))
-            .build());
-        var tableInsId = InstanceIdentifierUtils.generateFlowTableInstanceIdentifier(ncRef, new TableKey(NUM_ID_1));
-        assertNotNull(tableInsId);
-        assertEquals(NUM_ID_1, tableInsId.getFirstKeyOf(Table.class).getId());
+        assertEquals(InstanceIdentifier.builder(Nodes.class)
+            .child(Node.class, new NodeKey(new NodeId("nodeId")))
+            .child(NodeConnector.class, new NodeConnectorKey(new NodeConnectorId("ncId")))
+            .build(), InstanceIdentifierUtils.createNodeConnectorIdentifier("nodeId", "ncId"));
     }
 
     @Test
     void testGenerateTopologyInstanceIdentifier() {
-        var topologyInsId = InstanceIdentifierUtils.generateTopologyInstanceIdentifier(STR_ID_1);
-        assertNotNull(topologyInsId);
-        assertEquals(STR_ID_1, topologyInsId.getFirstKeyOf(Topology.class).getTopologyId().getValue());
+        assertEquals(InstanceIdentifier.builder(NetworkTopology.class)
+            .child(Topology.class, new TopologyKey(new TopologyId("topo")))
+            .build(), InstanceIdentifierUtils.generateTopologyInstanceIdentifier("topo"));
     }
 }
