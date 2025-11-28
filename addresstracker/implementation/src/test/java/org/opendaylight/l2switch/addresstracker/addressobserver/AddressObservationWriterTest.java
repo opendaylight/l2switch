dbb8b8366605da2ed9290616473d9b849c9bc8de
Robert Varga <robert.varga@pantheon.tech>
2025-11-28 21:00:59 +0100

    Modernize AddressObservationWriterTest
    Use JUnit5 with MockitoExtension and replace data object mocking with
    proper builders.
    
    Change-Id: I8e8b90b4b1648ccae4bd1cd62e5b114a5504b4cf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- addresstracker/implementation/src/test/java/org/opendaylight/l2switch/addresstracker/addressobserver/AddressObservationWriterTest.java
+++ addresstracker/implementation/src/test/java/org/opendaylight/l2switch/addresstracker/addressobserver/AddressObservationWriterTest.java
@@ -8,89 +8,86 @@
 package org.opendaylight.l2switch.addresstracker.addressobserver;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import com.google.common.util.concurrent.FluentFuture;
-import java.util.Map;
 import java.util.Optional;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.ReadTransaction;
 import org.opendaylight.mdsal.binding.api.WriteTransaction;
+import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.AddressCapableNodeConnector;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.address.node.connector.Addresses;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.address.node.connector.AddressesKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.AddressCapableNodeConnectorBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.address.node.connector.AddressesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
 import org.opendaylight.yangtools.binding.DataObjectIdentifier;
+import org.opendaylight.yangtools.binding.util.BindingMap;
 import org.opendaylight.yangtools.util.concurrent.FluentFutures;
+import org.opendaylight.yangtools.yang.common.Uint64;
 
-public class AddressObservationWriterTest {
+@ExtendWith(MockitoExtension.class)
+class AddressObservationWriterTest {
+    private final MacAddress macAddress = new MacAddress("ba:43:52:ce:09:f4");
+    private final IpAddress ipAddress = new IpAddress(new Ipv4Address("10.0.0.1"));
+    private final NodeConnectorId ncId = new NodeConnectorId("foo");
+    private final NodeConnectorRef realNcRef = new NodeConnectorRef(DataObjectIdentifier.builder(Nodes.class)
+        .child(Node.class, new NodeKey(new NodeId("abc")))
+        .child(NodeConnector.class, new NodeConnectorKey(ncId))
+        .build());
 
-    private AddressCapableNodeConnector addrCapableNc;
+    @Mock
+    private DataBroker dataBroker;
+    @Mock
     private ReadTransaction readTransaction;
+    @Mock
     private WriteTransaction writeTransaction;
-    private DataBroker dataService;
-    private FluentFuture<Optional<NodeConnector>> checkedFuture;
-    private NodeConnector nodeConnector;
-    private Addresses address;
-    private AddressesKey addrKey;
-    private MacAddress macAddress;
-    private IpAddress ipAddress;
-    private NodeConnectorRef realNcRef;
 
-    @Before
-    public void init() throws Exception {
-        macAddress = new MacAddress("ba:43:52:ce:09:f4");
-        ipAddress = new IpAddress(new Ipv4Address("10.0.0.1"));
-        realNcRef = new NodeConnectorRef(DataObjectIdentifier.builder(Nodes.class)
-            .child(Node.class, new NodeKey(new NodeId("abc")))
-            .child(NodeConnector.class, new NodeConnectorKey(new NodeConnectorId("foo")))
-            .build());
+    private AddressObservationWriter addressObservationWriter;
 
-        readTransaction = mock(ReadTransaction.class);
-        dataService = mock(DataBroker.class);
-        nodeConnector = mock(NodeConnector.class);
-        when(dataService.newReadOnlyTransaction()).thenReturn(readTransaction);
-        checkedFuture = FluentFutures.immediateFluentFuture(Optional.of(nodeConnector));
-        when(readTransaction.read(any(LogicalDatastoreType.class), any(DataObjectIdentifier.class)))
-                .thenReturn(checkedFuture);
-
-        addrCapableNc = mock(AddressCapableNodeConnector.class);
-        when(nodeConnector.augmentation(AddressCapableNodeConnector.class)).thenReturn(addrCapableNc);
-
-        address = mock(Addresses.class);
-        when(address.getIp()).thenReturn(ipAddress);
-        when(address.getMac()).thenReturn(macAddress);
-        when(address.getLastSeen()).thenReturn(1410350400L);
-        when(address.getFirstSeen()).thenReturn(1410350400L);
-        addrKey = mock(AddressesKey.class);
-        when(address.key()).thenReturn(addrKey);
-        when(addrCapableNc.getAddresses()).thenReturn(Map.of(addrKey, address));
-
-        writeTransaction = mock(WriteTransaction.class);
-        when(dataService.newWriteOnlyTransaction()).thenReturn(writeTransaction);
-        when(writeTransaction.commit()).thenReturn(mock(FluentFuture.class));
+    @BeforeEach
+    void beforeEach() {
+        addressObservationWriter = new AddressObservationWriter(dataBroker);
+        addressObservationWriter.setTimestampUpdateInterval(20L);
     }
 
     @Test
-    public void addAddressTest() throws Exception {
-        AddressObservationWriter addressObservationWriter = new AddressObservationWriter(dataService);
-        addressObservationWriter.setTimestampUpdateInterval(20L);
+    void addAddressTest() throws Exception {
+        doReturn(readTransaction).when(dataBroker).newReadOnlyTransaction();
+        doReturn(FluentFutures.immediateFluentFuture(Optional.of(
+            new NodeConnectorBuilder()
+            .setId(ncId)
+            .addAugmentation(new AddressCapableNodeConnectorBuilder()
+                .setAddresses(BindingMap.of(new AddressesBuilder()
+                    .setId(Uint64.ONE)
+                    .setIp(ipAddress)
+                    .setMac(macAddress)
+                    .setLastSeen(1410350400L)
+                    .setFirstSeen(1410350400L)
+                    .build()))
+                .build())
+            .build()))).when(readTransaction).read(any(LogicalDatastoreType.class), any(DataObjectIdentifier.class));
+
+        doReturn(writeTransaction).when(dataBroker).newWriteOnlyTransaction();
+        doReturn(CommitInfo.emptyFluentFuture()).when(writeTransaction).commit();
+
         addressObservationWriter.addAddress(macAddress, ipAddress, realNcRef);
         verify(readTransaction, times(1)).read(any(LogicalDatastoreType.class), any(DataObjectIdentifier.class));
         verify(readTransaction, times(1)).close();
@@ -100,10 +97,7 @@ public class AddressObservationWriterTest {
     }
 
     @Test
-    public void addAddressNullTest() throws Exception {
-
-        AddressObservationWriter addressObservationWriter = new AddressObservationWriter(dataService);
-        addressObservationWriter.setTimestampUpdateInterval(20L);
+    void addAddressNullTest() throws Exception {
         addressObservationWriter.addAddress(macAddress, null, realNcRef);
         verify(readTransaction, times(0)).read(any(LogicalDatastoreType.class), any(DataObjectIdentifier.class));
         verify(readTransaction, times(0)).close();
