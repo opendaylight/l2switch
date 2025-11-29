8257f756f910b85fa699e5f6ca8c35ef8b19645a
Robert Varga <robert.varga@pantheon.tech>
2025-11-29 22:52:54 +0100

    De-anonymize packethandler threads
    Provide a ThreadFactory to ensure that:
    - threads are properly named
    - threads do not block JVM shutdown
    - uncaught exceptions are recovered from
    
    Since we are in the area:
    - expand the FIXME around thread pool sharing, including possible
      future directions
    - shut down the threadpool without waiting for tasks to be processed
    
    Change-Id: I4b473939e3d7cff26e7961d614910d376bbebb41
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- packethandler/implementation/src/main/java/org/opendaylight/l2switch/packethandler/PacketListener.java
+++ packethandler/implementation/src/main/java/org/opendaylight/l2switch/packethandler/PacketListener.java
@@ -11,6 +11,7 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.mdsal.binding.api.NotificationService;
@@ -34,6 +35,7 @@ final class PacketListener<
         P extends Notification<P> & DataObject & PacketChainGrp & PacketPayload>
         implements NotificationService.Listener<C> {
     private static final Logger LOG = LoggerFactory.getLogger(PacketListener.class);
+    private static final AtomicLong POOL_COUNTER = new AtomicLong();
 
     private final @NonNull NotificationPublishService notificationPublishService;
     private final @NonNull AbstractDecoder<C, P> decoder;
@@ -47,8 +49,40 @@ final class PacketListener<
         requireNonNull(notificationService);
         this.decoder = requireNonNull(decoder);
 
-        // FIXME: use a better/shared executor
-        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        // FIXME: This is not nice: we are creating a fixed-size thread pool with normal priority competing for all
+        //        available cores and we do that for every decoder -- i.e. currently we are spawning 5xCORES threads.
+        //        These threads serve a dual purpose:
+        //          - they offload packet processing from the NotificationService pool, which is what we need to do
+        //            based on the contract of that service
+        //          - they allow for blocking (see onNotification() below), which is probably not what we want
+        //
+        //        This is also a side-effect of us wiring FirstDecoder and downstream SubsequentDecoder(s) via
+        //        Notification(Publish)Service. That in and of itself is causing unnecessary thread ping-pongs, for
+        //        example in the case of IPv4 ICMP:
+        //          1. we receive PacketReceived on this thread EthernetDecoder thread pool
+        //          2. we decode the packet and push it into NotificationPublishService (which is a different thread)
+        //          3. we receive the notification on Ipv4Decoder thread pool
+        //          4. we decode the packet and push it into NotificationPublishService again
+        //          5. we receive the notification on IcmpDecoder thread pool
+        //          6. we decode the packet and push it into NotificationPublishService again
+        //        So we switch processing threads 6 times to decode a single packet, all of which are competing for
+        //        CPU cores, potentially blocking (3 times) on NotificationPublishService and causing potential wakeups
+        //        (3 times).
+        //
+        //        At the end of the day we should probably have lower-priority CORES threads which are fed by
+        //        NotificationService delivering FirstDecoder inputs and those threads process the entire pipeline
+        //        producing only the notifications DemandListeners indicate. Event publishing should be non-blocking
+        //        (i.e. dropping output when NotificationPublishService is clogged).
+        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors(),
+            Thread.ofPlatform()
+                // i.e. "l2switch-packethandler-0-EthernetDecoder-1"
+                .name("l2switch-packethandler-" + POOL_COUNTER.getAndIncrement() + "-"
+                    + decoder.getClass().getSimpleName() + "-", 0)
+                // uncaught exceptions are a cause for warning, we just end up losing a packet
+                .uncaughtExceptionHandler((thread, cause) -> LOG.warn("Thread {} failed unexpectedly", thread, cause))
+                // okay to terminate JVM
+                .daemon()
+                .factory());
 
         consumerReg = notificationService.registerListener(decoder.consumedType(), this, executor);
         LOG.debug("Started processing {}", decoder);
@@ -79,6 +113,9 @@ final class PacketListener<
             return;
         }
 
+        // FIXME: This call will block current thread if the queues are clogged up. At the end of the day, since we are
+        //        an L2 switch, we should be able to to just drop the packet.
+        //        This is quite related to the executor pool above and what it is meant to achieve.
         try {
             notificationPublishService.putNotification(decoded);
         } catch (InterruptedException e) {
@@ -91,7 +128,9 @@ final class PacketListener<
         if (consumerReg != null) {
             consumerReg.close();
             consumerReg = null;
+
+            final var nrTasks = executor.shutdownNow().size();
+            LOG.debug("Executor for {} shut down with {} pending tasks", decoder.getClass().getSimpleName(), nrTasks);
         }
-        executor.shutdown();
     }
 }
